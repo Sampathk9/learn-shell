cb6203eab3b5ac69199a5eca40638d52d43327c3
Sampath Kumar <sampathkm09@gmail.com>
2023-03-08 02:16:57 +0800

    Functions with input's

--- 06-special-args.sh
+++ 06-special-args.sh
@@ -1,6 +1,18 @@
+echo "Following values from script"
 echo "0 - $0"
 echo "1 - $1"
 echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+example() {
+echo "Following values from function"
+echo "0 - $0" #Not applicable for function
+echo "1 - $1"
+echo "2 - $2"
+echo "* - $*"
+echo "@ - $@"
+echo "# - $#"
+}
+example xyz 20
