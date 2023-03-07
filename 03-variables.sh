f4c53ef79279d7e282e40773ef8b152638bc0745
Sampath Kumar <sampathkm09@gmail.com>
2023-03-07 16:23:19 +0800

    Shell Scripting Topics

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,3 +4,8 @@ name=sampath
 city=hyderabad
 echo person name is $name
 echo person is from ${city}
+
+
+#Here the date will change according to system's date since a command is executing for date variable
+DATE=$(date +%F)
+echo Today date is ${DATE}, Have a great day!
