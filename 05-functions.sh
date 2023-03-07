e5b7da2224141ae5f8a0af2131462fb84e61d789
Sampath Kumar <sampathkm09@gmail.com>
2023-03-08 01:35:36 +0800

    Exit output

---
+++ 05-functions.sh
@@ -0,0 +1,12 @@
+#funtions are like variables
+#Variable - when we assign a name to set of data then it is a variable
+#Function - when we assign a name to set of commands then it is function
+
+#function declare
+example() {
+  echo I am a example function
+
+}
+
+#call a function
+example
