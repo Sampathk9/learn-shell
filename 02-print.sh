b39a8a64e60252fe8a344159cf25dfeb27aee0cf
Sampath Kumar <sampathkm09@gmail.com>
2023-03-07 16:05:04 +0800

    Shell Scripting Topics

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,12 @@
 echo Hello World
 
 echo "*** Hello ***"
+
+#color code syntax: echo -e "\e[COLmMESSAGE\[0m"
+
+echo -e "\e[31mMESSAGE\e[0m"
+echo -e "\e[32mMESSAGE\e[0m"
+echo -e "\e[33mMESSAGE\e[0m"
+echo -e "\e[34mMESSAGE\e[0m"
+echo -e "\e[35mMESSAGE\e[0m"
+echo -e "\e[36mMESSAGE\e[0m"
